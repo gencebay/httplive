d41010855cf58ee8746570142c391a0be0412db5
Gencebay Demir <gencebay.demir@gmail.com>
2020-03-17 11:39:12 +0300

    Update middleware.go

--- lib/middleware.go
+++ lib/middleware.go
@@ -17,7 +17,7 @@ const httpLiveFileProviderEnvKey = "HttpLiveFileProvider"
 // CORSMiddleware ...
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding, x-access-token")
